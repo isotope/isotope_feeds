fe8ad062eeee703693f67005ab533128426fede4
Andreas Schempp <andreas@schempp.ch>
2011-02-02 09:31:07 +0000

    Downloaded all translations from betatest

---
+++ languages/nl/tl_iso_config.php
@@ -0,0 +1,42 @@
+<?php if (!defined('TL_ROOT')) die('You can not access this file directly!');
+
+/**
+ * TYPOlight Open Source CMS
+ * Copyright (C) 2005-2010 Leo Feyer
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  Winans Creative 2009, Intelligent Spark 2010, iserv.ch GmbH 2010
+ * @author     Paul Kegel <paul@artified.nl>
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ */
+
+$GLOBALS['TL_LANG']['tl_iso_config']['addFeed'][0] = 'Voeg een productfeed toe';
+$GLOBALS['TL_LANG']['tl_iso_config']['addFeed'][1] = 'Genereer een feed van de producten, in Google Base, RSS 2.0 en meer. De Feed is gebaseerd op bestaande producten, en wordt ge-update wanneer een product wordt toegevoegd of aangepast.';
+$GLOBALS['TL_LANG']['tl_iso_config']['feedName'][0] = 'bestandsnaam van feedbestand';
+$GLOBALS['TL_LANG']['tl_iso_config']['feedName'][1] = 'Voeg hier de bestandsnaam van de feed in zonder ".xml". Dit wordt de bestandsnaam van de feed waaraan het type nog wordt toegevoegd. (bijv. products-googlebase.xml)';
+$GLOBALS['TL_LANG']['tl_iso_config']['feedBase'][0] = 'Basis Url van de Feed';
+$GLOBALS['TL_LANG']['tl_iso_config']['feedBase'][1] = 'Voeg hier de Url van de basis Url in.';
+$GLOBALS['TL_LANG']['tl_iso_config']['feedTitle'][0] = 'Feed Titel';
+$GLOBALS['TL_LANG']['tl_iso_config']['feedTitle'][1] = 'Voeg hier de feed titel in';
+$GLOBALS['TL_LANG']['tl_iso_config']['feedDesc'][0] = 'Omschrijving van de Feed';
+$GLOBALS['TL_LANG']['tl_iso_config']['feedDesc'][1] = 'Voeg een beschrijving toe van de Feed';
+$GLOBALS['TL_LANG']['tl_iso_config']['feedJumpTo'][0] = 'Productdetails pagina';
+$GLOBALS['TL_LANG']['tl_iso_config']['feedJumpTo'][1] = 'Selecteer de pagina waarop de productdetail-module staat, om de link te genereren.';
+$GLOBALS['TL_LANG']['tl_iso_config']['feedTypes'][0] = 'Type Feeds';
+$GLOBALS['TL_LANG']['tl_iso_config']['feedTypes'][1] = 'Selecteer het type van de feed om deze te genereren';
+$GLOBALS['TL_LANG']['tl_iso_config']['feed_legend'] = 'Feeds';
+
