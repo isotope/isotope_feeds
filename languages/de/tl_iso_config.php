31abdc2bb63bf648d43a0751775b3c6d65e3a473
Andreas Schempp <andreas@schempp.ch>
2011-11-18 16:47:03 +0000

    Sync language files from FTP to SVN

---
+++ languages/de/tl_iso_config.php
@@ -0,0 +1,52 @@
+<?php if (!defined('TL_ROOT')) die('You can not access this file directly!');
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2010 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  2009-2011 Isotope eCommerce Workgroup
+ * @author     Andreas Burg <ab@andreasburg.de>
+ * @author     Nikolas Runde <info@nrmedia.de>
+ * @author     Patrick Grob <grob@a-sign.ch>
+ * @author     Frank Berger <berger@mediastuff.de>
+ * @author     Angelica Schempp <aschempp@gmx.net>
+ * @author     Oliver Hoff <oliver@hoff.com>
+ * @author     Stefan Preiss <stefan@preiss-at-work.de>
+ * @author     Nina Gerling <gerling@ena-webstudio.de>
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ * @version    $Id$
+ */
+
+$GLOBALS['TL_LANG']['tl_iso_config']['addFeed'][0] = 'Produkt-Feeds hinzufügen';
+$GLOBALS['TL_LANG']['tl_iso_config']['addFeed'][1] = 'Erstelle eine Auswahl an Produkt-Feeds inklusive Google Base, RSS 2.0 und weiteren. Feeds basieren auf existierenden Produkten und werden aktualisiert sobald das Produkt bearbeitet wird.';
+$GLOBALS['TL_LANG']['tl_iso_config']['feedName'][0] = 'Namens-Präfix für Feed-Dateien';
+$GLOBALS['TL_LANG']['tl_iso_config']['feedName'][1] = 'Geben Sie den Feed-Namen ohne die Dateiendung ".xml" ein. Dies wird der Präfix des Feed-Typs ein (z. B. produkte-googlebase.xml).';
+$GLOBALS['TL_LANG']['tl_iso_config']['feedBase'][0] = 'Feed-Basislink';
+$GLOBALS['TL_LANG']['tl_iso_config']['feedBase'][1] = 'Geben Sie die URL des Feed-Basislinks ein.';
+$GLOBALS['TL_LANG']['tl_iso_config']['feedTitle'][0] = 'Feed-Basistitel';
+$GLOBALS['TL_LANG']['tl_iso_config']['feedTitle'][1] = 'Geben Sie den Titel des Feed-Basislinks ein.';
+$GLOBALS['TL_LANG']['tl_iso_config']['feedDesc'][0] = 'Feed-Basisbeschreibung';
+$GLOBALS['TL_LANG']['tl_iso_config']['feedDesc'][1] = 'Geben Sie die Beschreibung des Feed-Basislinks ein.';
+$GLOBALS['TL_LANG']['tl_iso_config']['feedJumpTo'][0] = 'Feed-Leserseite';
+$GLOBALS['TL_LANG']['tl_iso_config']['feedJumpTo'][1] = 'Geben Sie die URL der Leserseite an um den Link zu erstellen.';
+$GLOBALS['TL_LANG']['tl_iso_config']['feedTypes'][0] = 'Feed-Typen';
+$GLOBALS['TL_LANG']['tl_iso_config']['feedTypes'][1] = 'Wählen Sie die Feed-Typen die generiert werden sollen.';
+$GLOBALS['TL_LANG']['tl_iso_config']['feed_legend'] = 'Feeds';
+
